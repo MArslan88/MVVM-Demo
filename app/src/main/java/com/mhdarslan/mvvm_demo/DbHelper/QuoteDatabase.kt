c3a7931892ff4c71b1d39b9deb179132ceee15e3
Muhammad Arslan <gery_maz_555@hotmail.com>
2026-05-11 22:58:04 +0500

    Room Database setup

---
+++ app/src/main/java/com/mhdarslan/mvvm_demo/DbHelper/QuoteDatabase.kt
@@ -0,0 +1,32 @@
+package com.mhdarslan.mvvm_demo.DbHelper
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.mhdarslan.mvvm_demo.DAO.QuoteDao
+import com.mhdarslan.mvvm_demo.Models.Quote
+
+@Database(entities = [Quote::class], version = 1)
+abstract class QuoteDatabase : RoomDatabase(){
+
+    abstract fun quoteDao(): QuoteDao
+
+    companion object{
+        private var INSTANCE : QuoteDatabase? = null
+
+        fun getDatabase(context: Context) : QuoteDatabase{
+            if(INSTANCE == null){
+                synchronized(this){
+                    INSTANCE = Room.databaseBuilder(context,
+                        QuoteDatabase::class.java,
+                        "quote_database")
+                        .createFromAsset("quotes.db")
+                        .build()
+                }
+            }
+
+            return INSTANCE!!
+        }
+    }
+}
